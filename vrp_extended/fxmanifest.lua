ffc5d225b05f4908a88388a018d64802d29da67b
RainSync <75763087+RainSync1@users.noreply.github.com>
2022-02-14 01:38:02 +0000

    Update fxmanifest.lua

--- vrp_extended/fxmanifest.lua
+++ vrp_extended/fxmanifest.lua
@@ -2,8 +2,6 @@ fx_version "adamant"
 
 game "gta5"
 
-shared_scripts {"config.lua"}
-
 server_scripts {"server/*.lua", "common/modules/*.lua", "common/*.lua"}
 
 client_scripts {"client/*.lua", "client/modules/*.lua", "common/modules/*.lua", "common/*.lua"}
