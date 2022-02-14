a16c3ed3f4d2f6669d964e14dc5926087c485414
RainSync <75763087+RainSync1@users.noreply.github.com>
2022-02-14 01:40:09 +0000

    Update common.lua

--- vrp_extended/server/common.lua
+++ vrp_extended/server/common.lua
@@ -10,13 +10,6 @@ function getSharedObject()
 	return vRPEXT
 end
 
-RegisterServerEvent("vrpext:clientLog")
-AddEventHandler("vrpext:clientLog", function(msg)
-	if Config.EnableDebug then
-		print(("[^2TRACE^7] %s^7"):format(msg))
-	end
-end)
-
 RegisterServerEvent("vrpext:triggerServerCallback")
 AddEventHandler("vrpext:triggerServerCallback", function(name, requestId, ...)
 	local playerId = source
